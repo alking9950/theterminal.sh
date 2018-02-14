c139027e4e6dcd6f88f1797a3ae98f045ce227af
Bryce Boe <bbzbryce@gmail.com>
2018-02-13 21:56:15 -0800

    Add program to assist in issuing requests

---
+++ request.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import requests
+import sys
+import time
+
+HOST = 'http://theterminal.sh'
+
+
+def run_command(command):
+    response = requests.get('{}/repl?command={}'.format(HOST, command))
+    if response.status_code == 503:
+        time.sleep(0.5)
+        return run_command(command)
+
+    print('{} {} ETAG: {}'.format(response.status_code, response.request.url,
+                                  response.headers.get('etag', 'NONE')))
+    if response.status_code == 200:
+        if response.text != '<p>command not found.</p>':
+            print(repr(response.text))
+    return response
+
+
+def main():
+    run_command('su+root')
+
+
+if __name__ == '__main__':
+    sys.exit(main())
